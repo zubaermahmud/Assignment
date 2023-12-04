ff4d73405bb99a9a241958f478b6c4c136b976b4
zubaermahmud <zubaer.mahmud31@gmail.com>
2023-12-04 22:09:13 +0600

    commiting changes to main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
 
 }
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
