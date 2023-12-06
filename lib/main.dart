35897c46d3e5928f1b966ff6a0da5403d3bd7e10
zubaermahmud <zubaer.mahmud31@gmail.com>
2023-12-06 13:05:33 +0600

    adding second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
+  runApp(const MyApp());
 }
+
+
+
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
@@ -18,9 +21,28 @@ class FirstPage extends StatelessWidget{
   const FirstPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My first Flutter App"),
+          backgroundColor: Colors.green[500],
+        ),
+        body: const Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
 
-    )
-  };
+class SecondPage extends StatelessWidget{
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+
+    );
+  }
 }
