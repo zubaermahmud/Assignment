0f364b206786072d8e048b1782d4a90716758690
zubaermahmud <zubaer.mahmud31@gmail.com>
2023-12-07 10:33:20 +0600

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,39 @@ class FirstPage extends StatelessWidget{
         child: Column(
           children: [
             ListTile(
-              leading: Image.asset("assets/asus.png"),
+              leading: Image.asset('image/Asus.png'),
+              title: const Text("Asus TUF Gaming A15",style: TextStyle(fontSize: 20),),
+              subtitle: const Text("A laptop for high performance gaming"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondPage()),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue[900]),
+              ),
+              child: const Text("Buy Now"),
+            ),ListTile(
+              leading: Image.asset('image/Asus.png'),
+              title: const Text("Asus TUF Gaming A15",style: TextStyle(fontSize: 20),),
+              subtitle: const Text("A laptop for high performance gaming"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondPage()),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue[900]),
+              ),
+              child: const Text("Buy Now"),
+            ),ListTile(
+              leading: Image.asset('image/Asus.png'),
               title: const Text("Asus TUF Gaming A15",style: TextStyle(fontSize: 20),),
               subtitle: const Text("A laptop for high performance gaming"),
             ),
@@ -61,14 +93,87 @@ class SecondPage extends StatelessWidget{
   const SecondPage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text('Buy Now'),
-        backgroundColor: Colors.green[100],
+        title: const Text('Choose Payment Method'),
       ),
-      
+      body: Column(
+        children: [
+          const SizedBox(height: 16),
+          const Text('Choose Payment Method:', style: TextStyle(fontSize: 16)),
+          const SizedBox(height: 16),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              PaymentMethodButton(icon: 'image/Bkash.png'),
+              SizedBox(width: 16),
+              PaymentMethodButton(icon: 'image/Nagad.png'),
+              SizedBox(width: 16),
+              PaymentMethodButton(icon: 'image/Visa.png'),
+              SizedBox(width: 16),
+              PaymentMethodButton(icon: 'image/Mastercard.png'),
+            ],
+          ),
+          const SizedBox(height: 16),
+          DropdownButton<String>(
+            value: 'Select City for delivery',
+            onChanged: (value) {},
+            items: const [
+              DropdownMenuItem(
+                value: 'Select City for delivery',
+                child: Text('Select City for delivery'),
+              ),
+              DropdownMenuItem(
+                value: 'Dhaka',
+                child: Text('Dhaka'),
+              ),
+              DropdownMenuItem(
+                value: 'Chittagong',
+                child: Text('Chittagong'),
+              ),
+              DropdownMenuItem(
+                value: 'Rajshahi',
+                child: Text('Rajshahi'),
+              ),
+              DropdownMenuItem(
+                value: 'Barisal',
+                child: Text('Barisal'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go Back'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class PaymentMethodButton extends StatelessWidget {
+  const PaymentMethodButton({
+    Key? key,
+    required this.icon,
+  }) : super(key: key);
+
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Image.asset(icon), // Replace 'assets/bkash.png' with the actual path to your icon
+      ),
     );
   }
 }
